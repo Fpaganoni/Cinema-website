9db37e46cd4ee521de4d65e807b9f96c18c4842b
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2025-08-20 21:22:37 +0200

    Configured webpack to find html in vercerl(webpack)

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,5 +1,6 @@
 const Dotenv = require("dotenv-webpack");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./scripts/index.js",
@@ -11,6 +12,10 @@ module.exports = {
   plugins: [
     new Dotenv({
       path: path.resolve(__dirname, ".env"), // Asegura que apunta a tu archivo .env
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html", // Ruta a tu archivo index.html original
+      filename: "index.html", // Nombre del archivo de salida
     }),
   ],
 };
